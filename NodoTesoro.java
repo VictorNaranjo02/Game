3d17bb74233876f40b5bb4852f4d645040f40f2b
VictorNaranjo02 <128171695+VictorNaranjo02@users.noreply.github.com>
2025-07-20 11:41:36 -0500

    NodoTesoro.java

---
+++ NodoTesoro.java
@@ -0,0 +1,10 @@
+public class NodoTesoro {
+    Tesoro tesoro;
+    NodoTesoro izquierdo;
+    NodoTesoro derecho;
+
+    public NodoTesoro(Tesoro tesoro) {
+        this.tesoro = tesoro;
+    }
+}
+
